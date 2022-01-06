a75dc6ebf5ad6d30437de6877ed9003dd08bc25a
joelpark1337 <joelpark1337@gmail.com>
2022-01-06 01:10:35 -0800

    test

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo rm -rf /home/ubuntu/app/requirements.txt
 
 PID=$(ps -ef | grep "/app/app.py" | grep -v grep | awk '{ print $2 }' | xargs echo )
 
 echo "Killing PID: '$PID'"
-kill $PID
+sudo kill $PID
 
 sleep 2
 echo "Server stopped."
 
+sudo rm -rf /home/ubuntu/app/*
+sudo rm -rf /home/ubuntu/scripts/*
 exit 0
