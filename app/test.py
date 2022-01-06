7edd8882bc15bcdc9f7d9ea97a7d862fbb3d7847
joelpark1337 <joelpark1337@gmail.com>
2022-01-06 01:01:29 -0800

    test

--- app/test.py
+++ app/test.py
@@ -1,4 +1,17 @@
-from flask import Flask
+import unittest
+import app
+import requests
+import json
 
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+        self.host = 'http://localhost:8080'
+    
+    def test_answer(self):
+        response = self.app.get('/')
+        data = response.data
+        self.assertEqual(b"Application Test", data)
 
-print ("python test")
+if __name__ == '__main__':
+    unittest.main()
